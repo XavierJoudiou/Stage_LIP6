c026de0f571cdce2914c2543589f4b188041ad3e
xavier <xavier@xavier-laptop.(none)>
2010-04-09 14:45:41 +0200

    Modif readme et script maj SVN

---
+++ copyGittoSvn.sh
@@ -0,0 +1,8 @@
+#§/bin/sh
+rm -rf ~/LIP6_SVN_Stage/VirtualMobility/AMAM/xjoudiou/*
+cp -R ./Vocabulaire ~/LIP6_SVN_Stage/VirtualMobility/AMAM/xjoudiou
+cp -R ./Fiches_Lectures ~/LIP6_SVN_Stage/VirtualMobility/AMAM/xjoudiou
+cp -R ./src ~/LIP6_SVN_Stage/VirtualMobility/AMAM/xjoudiou
+cp copyGittoSvn.sh ~/LIP6_SVN_Stage/VirtualMobility/AMAM/xjoudiou
+cp new_paper.sh ~/LIP6_SVN_Stage/VirtualMobility/AMAM/xjoudiou
+cp README ~/LIP6_SVN_Stage/VirtualMobility/AMAM/xjoudiou
