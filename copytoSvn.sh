443a0c8130c926f412e97a6e1fec3f8aea201c47
xavier <xavier@xavier-laptop.(none)>
2010-04-09 14:19:35 +0200

    MAJ

--- copytoSvn.sh
+++ copytoSvn.sh
@@ -1,5 +1,7 @@
 #§/bin/sh
 
-cp -R ./Vocabulaire ~/LIP6_SVN_Stage/VirtualMobility/AMAM/xjoudiou
-cp -R ./Fiches_Lectures ~/LIP6_SVN_Stage/VirtualMobility/AMAM/xjoudiou
+cp -R ./Vocabulaire/*tex ~/LIP6_SVN_Stage/VirtualMobility/AMAM/xjoudiou
+cp ./Vocabulaire/Makefile ~/LIP6_SVN_Stage/VirtualMobility/AMAM/xjoudiou
+cp -R ./Fiches_Lectures/*tex ~/LIP6_SVN_Stage/VirtualMobility/AMAM/xjoudiou
+cp ./Fiches_Lectures/Makefile ~/LIP6_SVN_Stage/VirtualMobility/AMAM/xjoudiou
 cp -R ./src ~/LIP6_SVN_Stage/VirtualMobility/AMAM/xjoudiou
