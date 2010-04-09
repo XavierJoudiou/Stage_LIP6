dd3f55418382300421363c1edb48d9149de3ddad
xavier <xavier@xavier-laptop.(none)>
2010-04-09 13:57:37 +0200

    Modif depot

---
+++ copytoSvn.sh
@@ -0,0 +1,5 @@
+#§/bin/sh
+
+cp -R ./Vocabulaire ~/LIP6_SVN_Stage/VirtualMobility/AMAM/xjoudiou
+cp -R ./Fiches_Lectures ~/LIP6_SVN_Stage/VirtualMobility/AMAM/xjoudiou
+cp -R ./src ~/LIP6_SVN_Stage/VirtualMobility/AMAM/xjoudiou
