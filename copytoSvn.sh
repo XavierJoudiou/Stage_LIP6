7c1506248de7b4e826b3d2ac3970102c74cdb02a
xavier <xavier@xavier-laptop.(none)>
2010-04-09 14:24:34 +0200

    REDO

--- copytoSvn.sh
+++ copytoSvn.sh
@@ -1,7 +1,5 @@
 #§/bin/sh
 
-cp -R ./Vocabulaire/*tex ~/LIP6_SVN_Stage/VirtualMobility/AMAM/xjoudiou
-cp ./Vocabulaire/Makefile ~/LIP6_SVN_Stage/VirtualMobility/AMAM/xjoudiou
-cp -R ./Fiches_Lectures/*tex ~/LIP6_SVN_Stage/VirtualMobility/AMAM/xjoudiou
-cp ./Fiches_Lectures/Makefile ~/LIP6_SVN_Stage/VirtualMobility/AMAM/xjoudiou
+cp -R ./Vocabulaire ~/LIP6_SVN_Stage/VirtualMobility/AMAM/xjoudiou
+cp -R ./Fiches_Lectures ~/LIP6_SVN_Stage/VirtualMobility/AMAM/xjoudiou
 cp -R ./src ~/LIP6_SVN_Stage/VirtualMobility/AMAM/xjoudiou
