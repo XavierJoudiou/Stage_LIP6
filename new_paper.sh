3a526208f1dc02a7ee499357808808050048940c
xavier <xavier@xavier-laptop.(none)>
2010-04-12 18:13:04 +0200

    Modification emplacement fichier type Latex

--- new_paper.sh
+++ new_paper.sh
@@ -23,8 +23,8 @@ then
 	done
 
 	echo On va creer les fichiers pour le vocabulaire et pour la fiche de lecture correspondant a $1
-	cp ./Vocabulaire/Type_Vocabulaire.tex ./Vocabulaire/$1.tex
-	cp ./Fiches_Lectures/Fiche_Type.tex ./Fiches_Lectures/$1.tex
+	cp ./Ressources/LaTex/Type_Vocabulaire.tex ./Vocabulaire/$1.tex
+	cp ./Ressources/LaTex/Fiche_Type.tex ./Fiches_Lectures/$1.tex
 else 
 	echo Donner le nom d\'un article 
 	exit 1
