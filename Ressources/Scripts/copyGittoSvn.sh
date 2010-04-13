c4a23933f4fdc43a51db84fb2520abece4430c72
xavier <xavier@xavier-laptop.(none)>
2010-04-13 09:38:10 +0200

    Réorganisation des dossiers et modifs necessaires pour scripts

--- Ressources/Scripts/copyGittoSvn.sh
+++ Ressources/Scripts/copyGittoSvn.sh
@@ -2,6 +2,7 @@
 cp -R ./Vocabulaire ~/LIP6_SVN_Stage/VirtualMobility/AMAM/xjoudiou
 cp -R ./Fiches_Lectures ~/LIP6_SVN_Stage/VirtualMobility/AMAM/xjoudiou
 cp -R ./src ~/LIP6_SVN_Stage/VirtualMobility/AMAM/xjoudiou
+cp -R ./Ressources ~/LIP6_SVN_Stage/VirtualMobility/AMAM/xjoudiou
 cp copyGittoSvn.sh ~/LIP6_SVN_Stage/VirtualMobility/AMAM/xjoudiou
 cp new_paper.sh ~/LIP6_SVN_Stage/VirtualMobility/AMAM/xjoudiou
 cp README ~/LIP6_SVN_Stage/VirtualMobility/AMAM/xjoudiou
---
+++ Ressources/Scripts/new_paper.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+if [ $# -eq 1 ]
+then
+	echo On va verifier que cette fiche existe pas deja
+
+	for file in `ls /home/xavier/Stage_LIP6/Vocabulaire`
+	do
+	    if [ $file = $1.tex ] 
+	    then
+		echo La fiche de vocabulaire existe deja.
+		exit 1
+	    fi
+	done
+
+	for file in `ls /home/xavier/Stage_LIP6/Fiches_Lectures`
+	do
+	    if [ $file = $1.tex ] 
+	    then
+		echo La fiche de lecture existe deja.
+		exit 1
+	    fi
+	done
+
+	echo On va creer les fichiers pour le vocabulaire et pour la fiche de lecture correspondant a $1
+	cp /home/xavier/Stage_LIP6/Ressources/LaTex/Type_Vocabulaire.tex /home/xavier/Stage_LIP6/Vocabulaire/$1.tex
+	cp /home/xavier/Stage_LIP6/Ressources/LaTex/Fiche_Type.tex /home/xavier/Stage_LIP6/Fiches_Lectures/$1.tex
+	echo Créations OK
+else 
+	echo Donner le nom d\'un article 
+	exit 1
+fi 
