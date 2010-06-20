8a37025ec35fc676fa0ee502e8eafdde9ec34d8f
Xavier <xavier@xavier-desktop.(none)>
2010-06-20 23:02:31 +0200

    Correction de certaines fautes

--- Ressources/Scripts/new_paper.sh
+++ Ressources/Scripts/new_paper.sh
@@ -4,7 +4,7 @@ if [ $# -eq 1 ]
 then
 	echo On va verifier que cette fiche existe pas deja
 
-	for file in `ls /home/xavier/Stage_LIP6/Vocabulaire`
+	for file in `ls /home/xavier/Git_Lip6/Stage_LIP6/Vocabulaire`
 	do
 	    if [ $file = $1.tex ] 
 	    then
@@ -13,7 +13,7 @@ then
 	    fi
 	done
 
-	for file in `ls /home/xavier/Stage_LIP6/Fiches_Lectures`
+	for file in `ls /home/xavier/Git_Lip6/Stage_LIP6/Fiches_Lectures`
 	do
 	    if [ $file = $1.tex ] 
 	    then
@@ -23,8 +23,8 @@ then
 	done
 
 	echo On va creer les fichiers pour le vocabulaire et pour la fiche de lecture correspondant a $1
-	cp /home/xavier/Stage_LIP6/Ressources/LaTex/Type_Vocabulaire.tex /home/xavier/Stage_LIP6/Vocabulaire/$1.tex
-	cp /home/xavier/Stage_LIP6/Ressources/LaTex/Fiche_Type.tex /home/xavier/Stage_LIP6/Fiches_Lectures/$1.tex
+	cp /home/xavier/Git_Lip6/Stage_LIP6/Ressources/LaTex/Type_Vocabulaire.tex /home/xavier/Git_Lip6/Stage_LIP6/Vocabulaire/$1.tex
+	cp /home/xavier/Git_Lip6/Stage_LIP6/Ressources/LaTex/Fiche_Type.tex /home/xavier/Git_Lip6/Stage_LIP6/Fiches_Lectures/$1.tex
 	echo Créations OK
 else 
 	echo Donner le nom d\'un article 
